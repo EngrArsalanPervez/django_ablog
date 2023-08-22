6bf7952233df31493a67e8268f360df293cdda71
EngrUsmanPervez <usman.pervez@seecs.edu.pk>
2023-08-22 22:58:41 +0500

    Update

--- theblog/views.py
+++ theblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from theblog.models import Post
 
@@ -17,3 +17,9 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
