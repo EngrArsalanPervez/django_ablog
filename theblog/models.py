9c888f2f0017055ec7868c1d34c5e9032b63b59d
EngrUsmanPervez <usman.pervez@seecs.edu.pk>
2023-08-22 16:41:37 +0500

    First Commit

--- theblog/models.py
+++ theblog/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(default="default", max_length=255)
+    title_tag = models.CharField(default="default", max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
