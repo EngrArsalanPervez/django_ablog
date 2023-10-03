6b9d38525b40d80a9cce8a7e46d6eb58d5a988a1
EngrUsmanPervez <usman.pervez10@gmail.com>
2023-10-03 19:14:16 +0500

    Duplicate line removed

--- theblog/models.py
+++ theblog/models.py
@@ -19,7 +19,6 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
